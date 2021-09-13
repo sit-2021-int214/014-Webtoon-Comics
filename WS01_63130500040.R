e7016172136065ce6cfb96be745779dbaadc4a94
Tripuwarej <68810388+Tripuwarej@users.noreply.github.com>
2021-09-13 16:28:44 +0700

    Add files via upload

---
+++ WS01_63130500040.R
@@ -0,0 +1,11 @@
+
+# Data Cleaning ------------------
+
+install.packages("readr")
+library(readr)
+
+install.packages("stringr")
+library(stringr)
+
+install.packages("assertive")
+library(assertive)
